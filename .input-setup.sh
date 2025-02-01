7262ddb79b73325d704e11dc4004b0f3536bedac
Jesus Gonzalez <jesus@jesusmg.net>
2025-02-01 17:13:11 -0300

    [Input setup] Enable middle button scrolling.

--- .input-setup.sh
+++ .input-setup.sh
@@ -5,5 +5,6 @@
 xrdb -merge ~/.Xresources
 xinput set-prop "Logitech USB Receiver Mouse" 308 -0.101
 xinput set-prop "Logitech USB Receiver Mouse" 170 1.1, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0
+xinput set-prop "Logitech USB Receiver Mouse" 300 0, 0, 1
 setxkbmap -layout us -variant altgr-intl
 xset r rate 240 36 # keyboard repeat delay and rate
