815e381cb99129d51745495bc15a63d19951606e
Jesus Gonzalez <jesus@jesusmg.net>
2025-02-03 21:32:57 -0300

    [Xinput] Use property names instead of ids.

--- .input-setup.sh
+++ .input-setup.sh
@@ -3,8 +3,10 @@
 # Keyboard and mouse setup
 
 xrdb -merge ~/.Xresources
-xinput set-prop "Logitech USB Receiver Mouse" 308 -0.101
-xinput set-prop "Logitech USB Receiver Mouse" 170 1.1, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0
-xinput set-prop "Logitech USB Receiver Mouse" 300 0, 0, 1
+
+xinput set-prop "Logitech USB Receiver Mouse" "Coordinate Transformation Matrix" 1.1, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0
+xinput set-prop "Logitech USB Receiver Mouse" "libinput Accel Speed" 0
+xinput set-prop "Logitech USB Receiver Mouse" "libinput Scroll Method Enabled"  0, 0, 1
+
 setxkbmap -layout us -variant altgr-intl
 xset r rate 240 36 # keyboard repeat delay and rate
