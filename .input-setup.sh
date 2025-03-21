b5df1729e5a20c3788d1e64696890c2b2c5c9d8d
Jesus Gonzalez <jesus@jesusmg.net>
2025-03-21 16:40:12 -0300

    [Xinput] Disable scroll wheel panning mode for desktop.

--- .input-setup.sh
+++ .input-setup.sh
@@ -6,7 +6,7 @@ xrdb -merge ~/.Xresources
 
 xinput set-prop "Logitech USB Receiver Mouse" "Coordinate Transformation Matrix" 1.1, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0
 xinput set-prop "Logitech USB Receiver Mouse" "libinput Accel Speed" 0
-xinput set-prop "Logitech USB Receiver Mouse" "libinput Scroll Method Enabled"  0, 0, 1
+xinput set-prop "Logitech USB Receiver Mouse" "libinput Scroll Method Enabled"  0, 0, 0
 
 setxkbmap -layout us -variant altgr-intl
 xset r rate 240 36 # keyboard repeat delay and rate
